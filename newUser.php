44a85058af10e3e92878c8c9e6799da5837ee11d
anariba-hn <anariba.hn@hotmail.com>
2018-04-28 15:19:50 -0600

    update php communication

--- newUser.php
+++ newUser.php
@@ -1,17 +1,15 @@
 <?php 
-header('Content-type: application/json; charset=utf8');
 include ("./connex.php"); //include db connection. import $cnn variable.
 
-if (($_POST['user_name'] != null) && ($_POST['user_email'] != null) && ($_POST['user_pw'] != null) && ($_POST['user_address']!= null)) 
-{
-	$user_name    = $_POST['user_name'];
-	$user_email   = $_POST['user_email'];
-	$user_pw      = $_POST['user_pw'];
-	$user_address = $_POST['user_address'];
-	$response= array();
-	$type    = 0; //user_type will be 0 by default as player 1 will be donate member	
-//	$cnn 	 = include 	
-
+    $user_name    = $_POST['user_name'];
+    $user_email   = $_POST['user_email'];
+    $user_pw      = $_POST['user_pw'];
+    $user_address = $_POST['user_address'];
+    $response= array();
+    $type    = 0; //user_type will be 0 by default as player 1 will be donate member
+//  $cnn     = include
+ if($user_name != null && $user_email != null && $user_pw != null && $user_address != null)   
+ {
 	$query = "SELECT * FROM users WHERE user_email = '$user_email' or user_address = '$user_address'";
 	if (!$result = mysqli_query($cnn, $query))
         exit(mysqli_error($conn));
@@ -37,7 +35,7 @@ if (($_POST['user_name'] != null) && ($_POST['user_email'] != null) && ($_POST['
          $response['message'] = "Succes !";
 
     }
-
+    header('Content-type: application/json; charset=utf8');
     echo json_encode($response);
 }
 else
